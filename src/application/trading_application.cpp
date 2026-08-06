8e75cf11f603340bf59b50fc2b68d817b7fc06d4
Jimmy Jordan <169525180+jimmyjordanSWE@users.noreply.github.com>
2026-08-06 15:27:18 +0200

    Populate watch list last price from current bar

--- src/application/trading_application.cpp
+++ src/application/trading_application.cpp
@@ -350,6 +350,8 @@ ApplicationUiSnapshot TradingApplication::SnapshotForUi(
                         *daily.previous_session_close;
                 if (daily.current_bar)
                     row_snapshot.session_open = daily.current_bar->open;
+                if (!row_snapshot.current_price && daily.current_bar)
+                    row_snapshot.current_price = daily.current_bar->close;
                 if (row_snapshot.current_price && daily.current_bar)
                     row_snapshot.change_from_open =
                         *row_snapshot.current_price - daily.current_bar->open;
